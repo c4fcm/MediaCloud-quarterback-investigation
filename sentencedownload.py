a34096dd6ab9341eac2411d40b749afb67bd69d4
Rahul Bhargava <rahulb@media.mit.edu>
2016-01-14 11:25:31 -0500

    page through results correctly

--- sentencedownload.py
+++ sentencedownload.py
@@ -1,9 +1,11 @@
-import datetime, json, string, logging, os
+import datetime, json, string, logging, os, codecs
 import unicodecsv as csv
 import textmining, mediacloud, stopwords
+from ConfigParser import SafeConfigParser
+
+ROWS_PER_QUERY = 500000
 
 # Load config data
-from ConfigParser import SafeConfigParser
 parser = SafeConfigParser()
 parser.read('config.txt')
 MY_API_KEY = parser.get('API','MY_API_KEY')
@@ -31,14 +33,34 @@ def fetch_corpus_from_mc(team,qb): #MC query, returns list of words
     qb_split = qb.split()
     exclude = list(string.punctuation)+qb_split+team.split()+byteify(stopwords)+['1','2','3','4','5','6','7','8','9','0']
     exclude = [x.lower() for x in exclude]
-    sentences = mc.sentenceList(solr_query=str('"'+qb+'"'), 
-        solr_filter=[mc.publish_date_query(datetime.date(2015,9,9), datetime.date(2016,1,4)), 
-                     '+media_id:('+media_id_str+')'], rows = 10000)
+    more = True
+    start = 0
+    while more:
+        logging.debug('    starting at %d' % start)
+        sentences = mc.sentenceList(solr_query=str('"'+qb+'"'), 
+            solr_filter=[mc.publish_date_query(datetime.date(2015,9,9), datetime.date(2016,1,4)), 
+                         '+media_id:('+media_id_str+')'], rows = ROWS_PER_QUERY, start=0)
+        more = len(sentences['response']['docs'])==ROWS_PER_QUERY
+        start = start + ROWS_PER_QUERY
     logging.info('    found %d sentences',len(sentences['response']['docs']))
     response = sentences['response']
     docs = response['docs']
     logging.info('  done')
     return " ".join([d['sentence'] for d in docs])
+
+def load_qb_corpus(team, qb):
+    corpora_dir = os.path.join('data','corpora')
+    qb_corpora_path = os.path.join(corpora_dir,qb+'.txt')
+    if not os.path.exists(corpora_dir):
+        os.makedirs(corpora_dir)
+    if os.path.exists(qb_corpora_path):
+        f = codecs.open(qb_corpora_path, 'r', encoding='utf-8')
+        corpus = f.read()
+    else:
+        corpus = fetch_corpus_from_mc(team,qb) 
+        f = codecs.open(qb_corpora_path, 'w', encoding='utf-8')
+        f.write(corpus)
+    return corpus
 
 def get_and_write_data():
     '''
@@ -58,18 +80,21 @@ def get_and_write_data():
         qb = row[1]
         race = row[2]
         file_label = str(qb+' ('+team+')')
-        qb_corpus = fetch_corpus_from_mc(team,qb) 
+        qb_corpus = load_qb_corpus(team,qb) 
         tdm.add_doc(qb_corpus)
         tdm_names.append(qb)
         if race == 'white':
             white_doc += qb_corpus + " "
         else:
             other_doc += qb_corpus + " "
+    logging.info('done')
     # write the results
+    logging.info('Writing results...')
     write_csv(tdm_names, tdm.rows(cutoff=1), 'word_freq_by_quarterback.csv')
     race_tdm.add_doc(white_doc)
     race_tdm.add_doc(other_doc)
     write_csv(['white','other'], race_tdm.rows(cutoff=1), 'word_freq_by_race.csv')
+    logging.info('done')
 
 def write_csv(cols, doc_iterator, filename):
     '''
